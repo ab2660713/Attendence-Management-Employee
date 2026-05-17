d32d3be65399a5f9ab10f63d7ed34c7b0696261a
ab2660713 <you@example.com>
2026-05-17 22:05:48 +0530

    Update frontend API backend URL

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -5,5 +5,5 @@ const isLocalDev = ["localhost", "127.0.0.1", "::1"].includes(
 window.__APP_CONFIG__ = {
   API_BASE: isLocalDev
     ? "http://localhost:4000/api"
-    : "https://attendencesystem-g7cg.onrender.com/api",
+    : "https://attendence-management-employee-2.onrender.com/api",
 };
